4ac51cadd0c591d2271b6f962df5785abb63e94f
Joaquin-Silv4 <Jsilva3@mail.austral.edu.ar>
2026-05-21 18:21:26 -0300

    Update list_modify.py

--- list_modify.py
+++ list_modify.py
@@ -12,7 +12,11 @@ def put(value, lst):
         put("Blue", colors) -> 1
         # colors ahora es ["Red", "Blue", "Green"]
     """
-    return "ANSWER HERE"  # Remove this line and implement
+    for i, v in enumerate(lst):
+        if v == "":
+            lst[i] = value
+            return i
+    return -1
 
 
 def remove(value, lst):
@@ -26,4 +30,9 @@ def remove(value, lst):
         remove("Red", colors) -> 2
         # colors ahora es ["", "Green", "", "Blue"]
     """
-    return "ANSWER HERE"  # Remove this line and implement
+    count = 0
+    for i, v in enumerate(lst):
+        if v == value:
+            lst[i] = ""
+            count += 1
+    return count
